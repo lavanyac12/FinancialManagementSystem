35c849854695f51c81c1f219abb2b4a82627774d
unknown <juan.pires@torontomu.ca>
2025-11-21 23:42:10 -0500

    uploading

---
+++ backend/supabase_client.py
@@ -0,0 +1,15 @@
+import os
+from supabase import create_client, Client
+from supabase.client import ClientOptions
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(
+    url,
+    key,
+    options=ClientOptions(
+        postgrest_client_timeout=10,
+        storage_client_timeout=10,
+        schema="public",
+    )
+)
